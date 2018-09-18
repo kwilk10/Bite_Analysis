1a2eb3543a7e07ccbb55d8532ee03036edf133a9
Clayton Gerstner <cgrstnr@gmail.com>
2018-09-17 20:43:48 -0400

    tested and debugged batch processing
    This works, at least for the 3 pig data files I have, using the parameter
    file for them. Given cycle starts & ends from parameter files are done.
    Still need to do our own and try na.approx

--- batch_run.R
+++ batch_run.R
@@ -1,19 +1,14 @@
-# Settables -- Right up top
-
-PARENT <- '/home/cgerstner/dad/'
-PARAM_DIR <- paste0(PARENT, 'params/')
-DATA_DIR <- paste0(PARENT, 'data/')
-SAVE_DIR <- paste0(PARENT, 'prepared/')
+# Settables are on the bottom in the main run_all function.
 
 # Libraries
-library(zoo)
+library(zoo) # na.locf, na.approx
 
 # Helper functions
 animal_from_filename <- function(name) {
-  animal <- strsplit(name, c('-'))[2]
+  animal <- unlist(strsplit(name, c('-')))[2]
   # Strip plurality  TODO: make sure this works for all the aminals (sic).
   if (substr(animal, nchar(animal), nchar(animal)) == 's') {
-    animal <- substr(animal, 1, nchar(tst) - 1)
+    animal <- substr(animal, 1, nchar(animal) - 1)
   }
   animal
 }
@@ -26,34 +21,35 @@ animal_from_filename <- function(name) {
 
 # Can't shake the feeling I could have done this better, vectorized and more R-like, but it's fine.
 
-# TODO: test at all
 read_data <- function(path, datapath) {
   # Here we pull in all of the descriptor files from which we shall pull
   # in all the data files.
+
   for (param in list.files(path)) {
-    par_frame <-read.csv(paste0(path, param))
-    par_frame[par_Frame$Food == '',]$Food <- NA
+    par_frame <- read.csv(paste0(path, param))
+    par_frame[par_frame$Food == '',]$Food <- NA
     par_frame$Food <- na.locf(par_frame$Food)
-    seq_only <- par[as.character(par$Date) != ""]
+    par_frame$ID <- na.locf(par_frame$ID)
+    seq_only <- par_frame[as.character(par_frame$Date) != "",]
+
+    animal <- animal_from_filename(param)
     
     # iterate over rows in parameter file in different data files
-    for (row in 1:nrow(seq_only)) {
+    for (rw in 1:nrow(seq_only)) {
       # construct filename to read:
+      row <- seq_only[rw,]
       
-      # convert date format in parameter file to Ymd like filenames
-      datestamp <- format(as.Date(newframe$Date, '%m/%d/%Y'), format = '%Y%m%d')
-      # animal
-      animal <- animal_from_filename(param)
-      # subject #
+      datestamp <- format(as.Date(row$Date, '%m/%d/%Y'), format = '%Y%m%d')
+      
       individual <- row$Individual
-      # fewd
       food <- tolower(as.character(row$Food))
-      # treatmentity
+      id <- row$ID
       treated <- ifelse(row$Treatment == 0, "ctrl", "treated") #TODO: find out what "treated" actually is in the filenames
       
-      # regular expression match filename for this sequence
-      regex <- paste0(animal, individual, "_", datestamp, "_", treated, "_", food, '.*\\.csv')
+      regex <- paste0(animal, individual, "_", datestamp, "_", treated, "_", food, "_", id, '.*\\.csv')
       datfile <- list.files(path = datapath, pattern = regex)
+      
+      if (length(datfile) == 0) next
       
       # read it in
       newframe <- read.csv(paste0(datapath,  datfile))
@@ -63,20 +59,41 @@ read_data <- function(path, datapath) {
       newframe$food <- food
       newframe$sequence <- row$Sequence
       newframe$date <- datestamp
-      
-      cycle_frame <- subset(par_frame, 
-                            Sequence == row$Sequence & Food == food & Individual == individual, 
+
+      cycle_frame <- subset(par_frame,
+                            Sequence == row$Sequence & Food == row$Food & Individual == row$Individual,
                             c(start, end))
-      for (i in nrow(cycle_frame)) {
-        newframe[cycle_frame[i,"start"]:cycle_frame[i,"end"],]$orig_cycle <- i
+
+      vec <- numeric(nrow(newframe))
+      for (i in 1:nrow(cycle_frame)) {
+        # Making cycle numbers unique in case that makes something easier later
+        cyc <- ifelse(exists("ret_df"), max(ret_df$orig_cycle) + i, i)
+        rang <- c(cycle_frame[i, "start"]:cycle_frame[i, "end"])
+        vec[rang] <- cyc
       }
-      
-      # rbind, with minor trade of efficiency for readability
-      df <- ifelse(exists(df), newframe, rbind(df, newframe))
+      newframe$orig_cycle <- vec
+
+      if  (!exists("ret_df")) {
+        ret_df <- newframe
+      }
+      else {
+        ret_df <- rbind(ret_df, newframe)
+      }
     }
   }
-  df
+  ret_df
 }
 
+# Main function
+run_all <- function() {
+  # Settables
+  PARENT <- '/home/cgerstner/dad/'
+  PARAM_DIR <- paste0(PARENT, 'params/')
+  DATA_DIR <- paste0(PARENT, 'data/')
+  SAVE_DIR <- paste0(PARENT, 'prepared/')
+
+  df <- read_data(PARAM_DIR, DATA_DIR)
+  df
+}
 
 
